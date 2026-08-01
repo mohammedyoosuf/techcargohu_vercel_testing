b3eb62dc23760f3819beecd634c5f51df8bb5a7c
Techcargohub <yoosuf.20221550@iit.ac.lk>
2026-08-01 22:44:14 +0530

    Configure writable /tmp storage for Vercel

--- api/index.php
+++ api/index.php
@@ -1,4 +1,25 @@
 <?php
 
+// Create required directories in Vercel's writable /tmp storage
+$tmpDirs = [
+    '/tmp/storage/framework/views',
+    '/tmp/storage/framework/cache/data',
+    '/tmp/storage/framework/sessions',
+    '/tmp/storage/logs',
+    '/tmp/bootstrap/cache',
+];
+
+foreach ($tmpDirs as $dir) {
+    if (!file_exists($dir)) {
+        mkdir($dir, 0755, true);
+    }
+}
+
+// Direct Laravel storage and view paths to /tmp
+putenv('APP_STORAGE_PATH=/tmp/storage');
+$_ENV['APP_STORAGE_PATH'] = '/tmp/storage';
+putenv('VIEW_COMPILED_PATH=/tmp/storage/framework/views');
+$_ENV['VIEW_COMPILED_PATH'] = '/tmp/storage/framework/views';
+
 // Forward incoming serverless requests to Laravel's public handler
 require __DIR__ . '/../public/index.php';
