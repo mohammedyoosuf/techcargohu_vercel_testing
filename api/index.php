b554d5b1754123a2139258119c3feda3405765e6
Techcargohub <yoosuf.20221550@iit.ac.lk>
2026-08-03 13:57:30 +0530

    Fix Vercel Laravel Statamic deployment

--- api/index.php
+++ api/index.php
@@ -1,5 +1,6 @@
-<?php
 
+<?php
+/*
 // Create required directories in Vercel's writable /tmp storage
 $tmpDirs = [
     '/tmp/storage/framework/views',
@@ -7,6 +8,7 @@ $tmpDirs = [
     '/tmp/storage/framework/sessions',
     '/tmp/storage/logs',
     '/tmp/bootstrap/cache',
+    '/tmp/storage/statamic/stache', // added newly
 ];
 
 foreach ($tmpDirs as $dir) {
@@ -23,3 +25,30 @@ $_ENV['VIEW_COMPILED_PATH'] = '/tmp/storage/framework/views';
 
 // Forward incoming serverless requests to Laravel's public handler
 require __DIR__ . '/../public/index.php';
+
+ */
+
+
+error_reporting(E_ALL);
+ini_set('display_errors', 1);
+
+$tmpDirs = [
+    '/tmp/storage/framework/views',
+    '/tmp/storage/framework/cache/data',
+    '/tmp/storage/framework/sessions',
+    '/tmp/storage/logs',
+    '/tmp/storage/statamic/stache',
+    '/tmp/bootstrap/cache',
+];
+
+foreach ($tmpDirs as $dir) {
+    if (!is_dir($dir)) {
+        mkdir($dir, 0755, true);
+    }
+}
+
+putenv('VIEW_COMPILED_PATH=/tmp/storage/framework/views');
+$_ENV['VIEW_COMPILED_PATH'] = '/tmp/storage/framework/views';
+
+require __DIR__ . '/../public/index.php';
+
